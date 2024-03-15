e0a38aa9d728f4a66ad2b5208d5ef5a536433371
André Cirillo <andreecirillo@gmail.com>
2024-03-15 09:42:45 -0300

    Project setup, basic components, handling elements states, styling pseudo elements and dark mode

---
+++ src/components/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="p-3 text-white bg-blue-600 rounded hover:bg-blue-900 transition-colors duration-500">
+        <slot></slot>
+    </button>
+</template>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="shadow-lg p-5 rounded-lg bg-gray-100">
+        <img class="rounded-md mb-4 mx-auto" src="/cat.jpg" alt="cat"/>
+        <h4 class="font-extralight text-2xl">Card Title</h4>
+        <hr class="bg-gray-400 my-2 h-[2px]">
+        <p class="line-clamp-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Officiis sint voluptates commodi dolorum facere consequatur nobis?
+            In corporis voluptatem dolorem distinctio cum, iste corrupti?
+            Consequuntur doloremque soluta at praesentium animi.
+        </p>
+    </div>
+</template>
