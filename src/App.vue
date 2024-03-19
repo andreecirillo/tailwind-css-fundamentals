031224082410d363e5087e79dd952ff861d33da5
André Cirillo <andreecirillo@gmail.com>
2024-03-19 19:08:21 -0300

    Pricing Table Challenge with Tailwind and Vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup language="ts">
-import Prose from './components/Prose.vue';
+import PricingCard from './components/PricingCard.vue';
 </script>
 <template>
-  <div class="m-10">
-    <Prose/>
+  <h1 class="text-3xl font-bold text-center m-5">Choose the best plan for your company</h1>
+  <h2 class="text-1xl text-center m-5">We are sure that one of them will fit your business best.</h2>
+  <div class="m-10 grid grid-cols-1 md:grid-cols-3 gap-5">
+    <PricingCard price="25" :features="[
+      'Unlimited Access to All Features',
+      '24/7 Customer Support',
+      'Customizable Dashboard',
+      'Advance Analytics Tools',
+      'Integration with Third-Party Services',
+      'Secure Data Encryption'
+    ]" name="Starter" description="Best option for personal use & for your next project." />
+    <PricingCard price="50" :features="[
+      'Multi-User Collaboration Tools',
+      'Real-Time Data Backup',
+      'Mobile App Access',
+      'Custom Branding Options',
+      'Automated Reporting System'
+    ]" name="Plus" description="Great for small teams and anyone looking to scale." />
+    <PricingCard price="150" :features="[
+      'Priority Feature Updates',
+      'Extended Data Storage Capacity',
+      'Personalized User Experience',
+      'In-Depth Training Materials',
+      'Dedicated Account Manager'
+    ]" name="Enterprise" description="Best for large scale uses and extended redistribution rights." />
   </div>
 </template>
---
+++ src/components/PricingCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    price: number;
+    features: string[];
+    name: string;
+    description: string;
+}>();
+</script>
+<template>
+    <div class="border rounded">
+        <div>
+            <h3 class="font-medium text-2xl text-center m-5">{{ name }}</h3>
+            <p class="text-center m-5">{{ description }}</p>
+            <div class="bg-[url('/checkmark.png')] text-center m-5">
+                <span class="font-medium text-5xl">${{ price }}</span>
+                <span>/month</span>
+            </div>
+            <ul role="list" class="list-disc marker:text-green-500 m-5 p-5">
+                <li class="space-h-5" v-for="feature in features" :key="feature">
+                    <span>{{ feature }}</span>
+                </li>
+            </ul>
+        </div>
+        <a class="bg-blue-500 hover:bg-blue-600 text-white text-center m-5 p-1 rounded block" href="#">Get started</a>
+    </div>
+</template>
